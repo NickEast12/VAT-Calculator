cdda9b843635fc40da1deadd71a8f4f74a81d212
NickEast12 <nickeast1998@gmail.com>
2018-10-15 18:14:27 +0100

    New look and now uses a form to gather user input

--- script.js
+++ script.js
@@ -1,51 +1,29 @@
-		
-			/*
-			var number = prompt('Enter the number you want to add VAT to');
-			var VAT = number / 100 * 20;
-			var VATadded = 1 * number + 1 * VAT;
-			alert(VATadded);
-			*/
-			/*
-			var number2 = prompt('Enter the number you wish to remove VAT from');
-			var VAT2 = number2 / 100 * 20;
-			alert(number2 - VAT2);
-			*/
-			
-			function myFunction() {
-				
-			// Assigning the varibles
-			alert('This is a working VAT calculator that can either add or take away different levels of VAT');
-			var numberInput = prompt('Please enter the figure that you wish to add or remove VAT');
-			var VATinput = prompt('Enter the tax rate you are using e.g 17.5 or 20');
-				
-		
-				
-				
-			// Makes sure the user enters in the correct phrase - eliminates the NaN error
-			for (var i=0; i !== 'takeaway' && i !== 'add';) {
-					i = prompt('Are you taking away VAT or Adding it? enter takeaway or add; no caps please');
-					
-				}
-			// the maths
-			if (i === 'takeaway') {
-				var workingOut = numberInput / 100 * VATinput;
-				var answer = numberInput - workingOut;
-				}
-			if (i === 'add') {
-					var workingOutTwo = numberInput / 100 * VATinput;
-					var answer2 = 1 * numberInput +  1 * workingOutTwo;
-				}	
-			// Pushing the inputted results to HTML
-			document.getElementById('YourNumber').innerHTML = '£' + numberInput;
-			document.getElementById('theTax').innerHTML = VATinput + ' %';
-			//Deciding on what answer to push to HTML 		
-			if (i === 'takeaway') {
-			document.getElementById('result').innerHTML = '£' + answer;
-				}
-			if (i === 'add') {
-				document.getElementById('result').innerHTML = '£' + answer2;
-				}
-			}
-			
-			
-					
+function myFunction() {
+// Gathering the user input
+var primeNumber = document.getElementById('Input').value;
+var VAT = document.getElementById('VATrate').value;
+var method = document.getElementById('method').value;
+// The maths based on what option the user selects	
+if (method === 'add') {
+	var removeVAT = primeNumber / 100 * VAT; 
+	var answer = 1 * primeNumber + 1 * removeVAT;
+}
+if (method === 'takeaway') {
+	var removeVAT2 = primeNumber / 100 * VAT;
+	var answer2 = 1 * primeNumber -  1 * removeVAT2;
+
+}
+
+
+	
+if (method === 'add') {
+	document.getElementById('answer').innerHTML = '£' + answer + '.00';
+	}
+if (method === 'takeaway') {
+	document.getElementById('answer').innerHTML = '£' + answer2 + '.00';
+	}
+ 
+return false;
+
+	
+}
