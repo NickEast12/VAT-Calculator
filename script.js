c1557f5e565a212c83131e175c88eaf75734655b
NickEast12 <nickeast1998@gmail.com>
2018-10-16 16:30:07 +0100

    minor updates

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function myFunction() {
-// Gathering the user input
-var primeNumber = document.getElementById('Input').value;
-var VAT; 
+	// Gathering the user input
+	var primeNumber = document.getElementById('Input').value;
+	
+	 if (isNaN(primeNumber)) {
+		alert('this is not a number')
+		
+
+	 }
+ 
+	
+	// Assigning the value of VAT depending on which VAT rate has been ticked
+	var VAT;
 	if (document.getElementById('VAT15').checked) {
   	VAT = document.getElementById('VAT15').value;
 	}
@@ -11,41 +20,31 @@ var VAT;
 	if (document.getElementById('VAT20').checked) {
   	VAT = document.getElementById('VAT20').value;
 	}
-	
-	
-var method;
+	// Assigning the method to the variable that has been checked by the user
+	var method;
 	if (document.getElementById('takeaway').checked) {
 		method = document.getElementById('takeaway').value;
 		}
 	if (document.getElementById('add').checked) {
 		method = document.getElementById('add').value;
 		}
-	
-//var method = document.getElementById('method').value;
-	
-	
-	
-	
-	
 // The maths based on what option the user selects	
-if (method === 'add') {
-	var removeVAT = primeNumber / 100 * VAT; 
+	if (method === 'add') {
+	var removeVAT = primeNumber / 100 * VAT;
 	var answer = 1 * primeNumber + 1 * removeVAT;
 }
-if (method === 'takeaway') {
+	if (method === 'takeaway') {
 	var removeVAT2 = primeNumber / 100 * VAT;
 	var answer2 = 1 * primeNumber -  1 * removeVAT2;
 
 }
 // Pushing the anwer to HTML	
-if (method === 'add') {
-	document.getElementById('answer').innerHTML = '£' + answer + '.00';
+	if (method === 'add') {
+	document.getElementById('answer').innerHTML = '£' + answer;
 	}
-if (method === 'takeaway') {
-	document.getElementById('answer').innerHTML = '£' + answer2 + '.00';
+	if (method === 'takeaway') {
+	document.getElementById('answer').innerHTML = '£' + answer2;
 	}
 // Stops the answer from disappearing as the page reloads 
-return false;
-
-	
+	return false;
 }
